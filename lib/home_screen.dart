da885e717abd2e529ea9f91da6582b7c451ff8f5
Nicopee <gidudunicholas11@gmail.com>
2024-01-17 13:24:35 +0300

    Added localization files

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:localization_app/class/language.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,12 +9,61 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Localization'),
+        backgroundColor: Colors.blue,
+        title: const Text('Localization App'),
+        // title: Text(translation(context).homePage),
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: DropdownButton<Language>(
+              underline: const SizedBox(),
+              icon: const Icon(
+                Icons.language,
+                color: Colors.white,
+              ),
+              onChanged: (Language? language) async {
+                if (language != null) {
+                  // Locale _locale = await setLocale(language.languageCode);
+                  // MyApp.setLocale(context, _locale);
+                }
+              },
+              items: Language.languageList()
+                  .map<DropdownMenuItem<Language>>(
+                    (e) => DropdownMenuItem<Language>(
+                      value: e,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Text(
+                            e.flag,
+                            style: const TextStyle(fontSize: 30),
+                          ),
+                          Text(e.name)
+                        ],
+                      ),
+                    ),
+                  )
+                  .toList(),
+            ),
+          ),
+        ],
       ),
-      body: const Center(
+      body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Text(AppLocalizations.of(context)!.about),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(AppLocalizations.of(context)!.dateOfBirth),
+            const SizedBox(
+              height: 10,
+            ),
             Text('Hello John Doe'),
+            SizedBox(
+              height: 20,
+            ),
             Text('I want my app in different languages'),
           ],
         ),
